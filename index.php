3ee59955199ca967c0534168c4268eac5286a17e
Darshan <DozzerCoder>
2018-03-18 11:56:28 +0530

    reading operation done

--- index.php
+++ index.php
@@ -11,13 +11,23 @@
   }else{
     echo "Database is connected";
   }
-
-    $sql = "INSERT INTO posts (tasks) VALUES ('$_POST[notes]')";
-    if(mysqli_query($conn,$sql)){
+//Values inserted to database
+    $insertData = "INSERT INTO posts (tasks) VALUES ('$_POST[notes]')";
+    if(mysqli_query($conn,$insertData)){
       echo "value is inserted";
     }else{
       echo "value is not inserted";
     }
+//Reading values from database
+      $readData = "SELECT id,tasks FROM posts";
+      $resultData = mysqli_query($conn,$readData);
+      if(mysqli_num_rows($resultData) > 0){
+        while($row = mysqli_fetch_assoc($resultData)){
+          echo "id: ".$row['id']. "Note: ".$row['tasks']."<br>";
+        }
+      }else{
+        echo " 0 results are inserted ";
+      }
   // $sql = "INSERT INTO posts(tasks) VALUES('Hello This is my first note!!')";
   // if(mysqli_query($conn,$sql)){
   //   echo" value inserted";
