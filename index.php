32269822659628716e2a805f8fadee1113d522c6
Darshan <DozzerCoder>
2018-03-18 11:33:41 +0530

    database value is isserted using buttons

--- index.php
+++ index.php
@@ -6,16 +6,18 @@
 
   //Checks the connection to database is established or not
   $conn = mysqli_connect($servername,$username,$password,$dbname);
-
-
-  if(isset($_POST['submit'])){
-    if(!empty($_POST['notes'])){
-      $sql = "INSERT INTO posts(tasks) VALUES ('task')";
-      echo "Data inserted succesfully";
-    }else{
-      echo "values is not inserted";
-    }
+  if(!$conn){
+    echo "Database is not connected ";
+  }else{
+    echo "Database is connected";
   }
+
+    $sql = "INSERT INTO posts (tasks) VALUES ('$_POST[notes]')";
+    if(mysqli_query($conn,$sql)){
+      echo "value is inserted";
+    }else{
+      echo "value is not inserted";
+    }
   // $sql = "INSERT INTO posts(tasks) VALUES('Hello This is my first note!!')";
   // if(mysqli_query($conn,$sql)){
   //   echo" value inserted";
