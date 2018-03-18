15833dde2e05d3bc48763ad49f98183dd4687724
Darshan <DozzerCoder>
2018-03-18 11:22:57 +0530

    database connection established

--- index.php
+++ index.php
@@ -7,9 +7,19 @@
   //Checks the connection to database is established or not
   $conn = mysqli_connect($servername,$username,$password,$dbname);
 
-  if(!$conn){
-    echo "Connection not established"
-  }else{
-    echo "Connection established"
+
+  if(isset($_POST['submit'])){
+    if(!empty($_POST['notes'])){
+      $sql = "INSERT INTO posts(tasks) VALUES ('task')";
+      echo "Data inserted succesfully";
+    }else{
+      echo "values is not inserted";
+    }
   }
+  // $sql = "INSERT INTO posts(tasks) VALUES('Hello This is my first note!!')";
+  // if(mysqli_query($conn,$sql)){
+  //   echo" value inserted";
+  // }else{
+  //   echo "value is not inserted";
+  // }
 ?>
